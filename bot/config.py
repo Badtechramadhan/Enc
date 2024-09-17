da8a81463cf1398a53adfdaeb08a2a74b4a121b8
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2024-09-17 23:02:13 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -33,7 +33,7 @@ class Config:
             self.BOT_TOKEN = config("BOT_TOKEN")
             self.CACHE_DL = config("CACHE_DL", default=False, cast=bool)
             self.CAP_DECO = config("CAP_DECO", default="◉")
-            self.C_LINK = config("C_LINK", default="@ANi_MiNE")
+            self.C_LINK = config("C_LINK", default="@FZXAnime")
             self.CMD_SUFFIX = config("CMD_SUFFIX", default=str())
             self.COMP_MODE = config("COMPATIBILITY_MODE", default=True, cast=bool)
             self.DATABASE_URL = config("DATABASE_URL", default=None)
@@ -53,7 +53,7 @@ class Config:
                 "FFMPEG",
                 default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
             )
-            self.FINISHED_PROGRESS_STR = config("FINISHED_PROGRESS_STR", default="🧡")
+            self.FINISHED_PROGRESS_STR = config("FINISHED_PROGRESS_STR", default="■")
             self.FL_CAP = config("FILENAME_AS_CAPTION", default=False, cast=bool)
             self.FS_THRESHOLD = config("FLOOD_SLEEP_THRESHOLD", default=600, cast=int)
             self.FSTICKER = config("FSTICKER", default=None)
@@ -88,7 +88,7 @@ class Config:
             self.TG_UL_CLIENT = config("TG_UL_CLIENT", default="pyrogram")
             self.THUMB = config("THUMBNAIL", default=None)
             self.UN_FINISHED_PROGRESS_STR = config(
-                "UN_FINISHED_PROGRESS_STR", default="🤍"
+                "UN_FINISHED_PROGRESS_STR", default="□"
             )
             self.USE_ANILIST = config("USE_ANILIST", default=True, cast=bool)
             self.USE_CAPTION = config("USE_CAPTION", default=True, cast=bool)
